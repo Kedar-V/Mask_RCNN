3a8d5742948b09ede372f7a1ee4eca1779e43bc7
Kedar-V <65109826+Kedar-V@users.noreply.github.com>
2020-11-14 17:19:57 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -925,13 +925,11 @@ def fpn_classifier_graph(rois, feature_maps, image_meta,
                         name="roi_align_classifier")([rois, image_meta] + feature_maps)
     # Two 1024 FC layers (implemented with Conv2D for consistency)
     x = KL.TimeDistributed(KL.Conv2D(fc_layers_size, (pool_size, pool_size), padding="valid"),name="mrcnn_class_conv1")(x)
-    x = KL.TimeDistributed(KL.Dropout(0.5), name="mrcnn_class_dp1")(x)
     x = KL.TimeDistributed(BatchNorm(), name='mrcnn_class_bn1')(x, training=train_bn)
     x = KL.Activation('relu')(x)
     
     x = KL.TimeDistributed(KL.Conv2D(fc_layers_size, (1, 1)),
                            name="mrcnn_class_conv2")(x)
-    x = KL.TimeDistributed(KL.Dropout(0.5), name="mrcnn_class_dp2")(x)
     x = KL.TimeDistributed(BatchNorm(), name='mrcnn_class_bn2')(x, training=train_bn)
     x = KL.Activation('relu')(x) 
     shared = KL.Lambda(lambda x: K.squeeze(K.squeeze(x, 3), 2),
